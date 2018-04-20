009a5427378248a32e8cbd32cc2bb63f24fafffe
Sami Konzo <samikonzo@gmail.com>
2018-04-20 19:36:16 +0300

    :create

---
+++ src/client/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const App = () => {return (<div> bebebe </div>)}
+
+export default App
---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import App from './client/App.jsx'
+import Html from './client/Html.js'
+
+const port = 3000
+const app = express()
+
+app.get('/', (req, res) => {
+
+	const body = renderToString(<App />);
+	const title = 'Server-side rendering with Styled Components'
+
+	res.send(
+		Html({body, title})
+	)	
+})
+
+app.listen(port)
